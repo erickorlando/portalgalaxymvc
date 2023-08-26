b75cff2cf7fa361a9f668816948e31d67d55a690
Erick Orlando <erickorlando@outlook.com>
2023-08-26 11:15:20 -0500

    Primer modelado de base de datos

--- PortalGalaxy.ApiRest/Program.cs
+++ PortalGalaxy.ApiRest/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PortalGalaxy.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<PortalGalaxyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GalaxyDatabase"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ PortalGalaxy.Entities/Categoria.cs
@@ -0,0 +1,7 @@
+﻿namespace PortalGalaxy.Entities
+{
+    public class Categoria : EntityBase
+    {
+        public string Nombre { get; set; } = default!;
+    }
+}
