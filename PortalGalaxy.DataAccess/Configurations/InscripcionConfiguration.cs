015d936f5b0a69f7eece26baba2cb6915bf1d5d6
Erick Orlando <erickorlando@outlook.com>
2023-08-26 11:48:18 -0500

    Tablas adicionales para la aplicacion

---
+++ PortalGalaxy.DataAccess/Configurations/InscripcionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortalGalaxy.Entities;
+
+namespace PortalGalaxy.DataAccess.Configurations
+{
+    public class InscripcionConfiguration : IEntityTypeConfiguration<Inscripcion>
+    {
+        public void Configure(EntityTypeBuilder<Inscripcion> builder)
+        {
+            builder.Property(p => p.Situacion)
+                .HasColumnType("smallint");
+        }
+    }
+
+}
